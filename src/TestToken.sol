193a6ef0919ccda28b5bb05420c1bcb975c1054d
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-17 13:03:54 +0100

    Deply script + Makefile

--- src/TestToken.sol
+++ src/TestToken.sol
@@ -7,6 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract TestToken is ERC20{
     
     constructor(uint initialSupply) ERC20("TestToken","TT"){
-        
+        _mint(msg.sender, initialSupply);
     }
 }
