1ce54a4edf1eded1ab357d929908073deac8305e
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-19 17:39:27 +0100

    Creating test, currently empty

---
+++ test/TestTokenTest.t.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {TestToken} from "../src/TestToken.sol";
+
+contract TestTokenTest{
+
+    function setUp() public{
+        
+    }
+}
