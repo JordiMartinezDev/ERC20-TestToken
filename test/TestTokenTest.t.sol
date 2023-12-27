57892084e109910d08dd00cc3e4b50ff594bf70b
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-27 10:40:36 +0100

    Test initial supply fixed

--- test/TestTokenTest.t.sol
+++ test/TestTokenTest.t.sol
@@ -120,7 +120,7 @@ contract TestTokenTest is Test {
     }
     function testTotalSupply() public {
     // Test the total supply of the token
-    assertEq(testToken.totalSupply(), STARTING_BALANCE, "Incorrect total supply");
+    assertEq(testToken.totalSupply(), 1000 ether, "Incorrect total supply");
     }
     function testReentrancyAttack() public {
         // Reentrancy attack test
